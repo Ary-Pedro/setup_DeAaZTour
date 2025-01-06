44e193cc7dc0ac6469bd26cbac7b5edb0047bd95
Pedro Cézar <129360971+Ary-Pedro@users.noreply.github.com>
2025-01-06 18:46:25 -0300

    Delete Sistema de DeaAz directory

--- Sistema de DeaAz/homeAdmin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CadadminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homeAdmin'
--- Sistema de DeaAz/homeFuncionario/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomefuncionarioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homeFuncionario'
