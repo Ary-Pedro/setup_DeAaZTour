d76b63c5120c63cd3994b1cc0b2c113924af52b2
AryPedro <pedrocssolza@gmail.com>
2025-01-15 03:03:17 -0300

    Update urls.py

--- setup_DeAaZTour/urls.py
+++ setup_DeAaZTour/urls.py
@@ -5,5 +5,7 @@ from django.apps import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.worker.urls.py")),
-
+    path("", include("apps.agency.urls.py")),
+    path("", include("apps.client.urls.py")),
+    path("", include("apps.service.urls.py")),
 ]
